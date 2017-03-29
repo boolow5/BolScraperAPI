594494ac377db0e0b9c8fcf32e4f6859cc7810e4
Mahdi Bolow <boolow5@gmail.com>
2017-03-29 19:05:07 +0300

    removed print func

--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/boolow5/BolScraperAPI/controllers"
 	"gopkg.in/gin-gonic/gin.v1"
 )
 
 func main() {
-	fmt.Println("Hello World")
 	router := gin.Default()
 	router.GET("/", controllers.Index)
 	router.POST("/fetch/list", controllers.Scrape)
+	router.POST("/fetch/detail", controllers.GetDetail)
 	router.Run()
 }
